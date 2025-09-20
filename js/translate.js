d29f8a46cad8b15db3ff90685e5b3eb24a76cda0
LUZ MORENA SILVA SÁNCHEZ <59208215@ceibal.edu.uy>
2025-09-20 18:38:54 -0300

    Arreglo botón de suscripciones

--- js/translate.js
+++ js/translate.js
@@ -1,21 +1,28 @@
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'es',
-    includedLanguages: 'es,en',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-    autoDisplay: false
-  }, 'google_translate_element');
+    new google.translate.TranslateElement({
+        pageLanguage: 'es',
+        includedLanguages: 'es,en',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false
+    }, 'google_translate_element');
 }
 
 function cambiarIdioma(lang) {
-  document.cookie = "googtrans=/es/" + lang + ";path=/";
-  location.reload();
+    document.cookie = "googtrans=/es/" + lang + ";path=/";
+    location.reload();
 }
 
-// Cargar el script de Google Translate dinámicamente
 (function() {
   var gtScript = document.createElement("script");
   gtScript.type = "text/javascript";
   gtScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
   document.head.appendChild(gtScript);
 })();
+
+ function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'es',
+                includedLanguages: 'es,en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'google_translate_element');
+        }
