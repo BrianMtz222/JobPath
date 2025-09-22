38db1b3b634458c58283d9f976f8da944ae06d16
LUZ MORENA SILVA SÁNCHEZ <59208215@ceibal.edu.uy>
2025-09-22 16:30:49 -0300

    Arreglo

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,32 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtenemos los contenedores de tarjetas
     const visibleCards = document.querySelector('.visible-cards');
     const hiddenCards = document.querySelector('.hidden-cards');
-    
-    // Obtenemos los botones de navegación
     const prevArrow = document.querySelector('.prev-arrow');
     const nextArrow = document.querySelector('.next-arrow');
 
-    // Inicialmente, las tarjetas "visibles" están activas
-    let isVisible = true;
+    let isVisible = true; // control de estado
 
-    // Función para manejar el clic en la flecha "siguiente"
     const showNext = () => {
-        // Si las tarjetas visibles están activas, las ocultamos y mostramos las nuevas
         if (isVisible) {
-            visibleCards.style.opacity = '0'; // Inicia la transición de desvanecimiento
+            visibleCards.style.transition = 'opacity 0.5s';
+            hiddenCards.style.transition = 'opacity 0.5s';
+
+            visibleCards.style.opacity = '0';
             setTimeout(() => {
-                visibleCards.style.display = 'none'; // Oculta el contenedor
-                hiddenCards.style.display = 'flex'; // Muestra el nuevo contenedor
-                hiddenCards.style.opacity = '1'; // Inicia la transición de aparición
-            }, 500); // Espera a que termine la transición de desvanecimiento
+                visibleCards.style.display = 'none';
+                hiddenCards.style.display = 'flex';
+                hiddenCards.style.opacity = '1';
+            }, 500);
             isVisible = false;
         }
     };
 
-    // Función para manejar el clic en la flecha "anterior"
     const showPrev = () => {
-        // Si las tarjetas ocultas están activas, las ocultamos y mostramos las originales
         if (!isVisible) {
             hiddenCards.style.opacity = '0';
             setTimeout(() => {
@@ -38,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Agregamos los escuchadores de eventos a los botones
     nextArrow.addEventListener('click', showNext);
     prevArrow.addEventListener('click', showPrev);
 });
